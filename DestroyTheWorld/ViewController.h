8da4d27a67d4c36ffeb14c275cd1a696455d813e
Saad Alzahrani <mekatous@gmail.com>
2016-10-19 16:52:26 +0300

    Initial Commit

---
+++ DestroyTheWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DestroyTheWorld
+//
+//  Created by saad alzahrani on 10/19/16.
+//  Copyright © 2016 saad alzahrani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DestroyTheWorld/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DestroyTheWorld
+//
+//  Created by saad alzahrani on 10/19/16.
+//  Copyright © 2016 saad alzahrani. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
